5544351f3f78a9896c9aaaf4ac98ff1ba3cd8058
Umit Uzulmez <umituzulmez@gmail.com>
2020-01-25 15:53:02 +0000

    TesCase

--- src/test/java/com/zerobank/pages/AccountActivityPage.java
+++ src/test/java/com/zerobank/pages/AccountActivityPage.java
@@ -25,10 +25,22 @@ public class AccountActivityPage extends BasePage{
     @FindBy(css = ".btn.btn-primary")
     public WebElement findButton;
 
+    @FindBy(id = "aa_description")
+    public WebElement descriptionBox;
+
+    @FindBy(id = "aa_type")
+    public WebElement typeDropdpown;
+
     public Select accountDropdownList(){
         WebElement accountDropdownMenu = Driver.get().findElement(By.id("aa_accountId"));
         Select selectList = new Select(accountDropdownMenu);
 
+        return selectList;
+    }
+
+    public Select selectTypeList(){
+        WebElement selectedType = Driver.get().findElement(By.id("aa_type"));
+        Select selectList = new Select(selectedType);
         return selectList;
     }
 
@@ -40,4 +52,12 @@ public class AccountActivityPage extends BasePage{
     public List<WebElement> transactionsHeaders = Driver.get().findElements(By.xpath("//*[@class='table table-condensed table-hover']/thead/tr/th"));
 
     public List<WebElement> dateColumnList = Driver.get().findElements(By.xpath("(//table[contains(@class,'table')])[2]//tbody/tr/td[1]"));
+
+    public List<WebElement> descriptionColumnList = Driver.get().findElements(By.xpath("(//table[contains(@class,'table')])[2]//tbody/tr/td[2]"));
+
+    public List<WebElement> depositColumnList = Driver.get().findElements(By.xpath("(//table[contains(@class,'table')])[2]//tbody/tr/td[3]"));
+
+    public List<WebElement> withdrawalColumnList = Driver.get().findElements(By.xpath("(//table[contains(@class,'table')])[2]//tbody/tr/td[4]"));
+
+
 }
--- src/test/java/com/zerobank/step_definitions/AccountActivityStepDef.java
+++ src/test/java/com/zerobank/step_definitions/AccountActivityStepDef.java
@@ -7,6 +7,7 @@ import com.zerobank.pages.LoginPage;
 import com.zerobank.utilities.BrowserUtils;
 import com.zerobank.utilities.ConfigurationReader;
 import com.zerobank.utilities.Driver;
+import io.cucumber.datatable.dependency.com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.cucumber.java.bs.A;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -40,17 +41,17 @@ public class AccountActivityStepDef {
         String selectedOption = accountActivityPage.accountDropdownList().getFirstSelectedOption().getText();
         String expectedOption = Option;
 
-        Assert.assertEquals(expectedOption,selectedOption);
+        Assert.assertEquals(expectedOption, selectedOption);
     }
 
     @Then("Account dropdown should have following options")
     public void account_dropdown_should_have_following_options(List<String> dropdownOptions) {
-       AccountActivityPage accountActivityPage = new AccountActivityPage();
-       List<WebElement> options = accountActivityPage.accountDropdownList().getOptions();
-       List<String> actualOptionList = BrowserUtils.getElementsText(options);
-       List<String> expectedOptionList = dropdownOptions;
+        AccountActivityPage accountActivityPage = new AccountActivityPage();
+        List<WebElement> options = accountActivityPage.accountDropdownList().getOptions();
+        List<String> actualOptionList = BrowserUtils.getElementsText(options);
+        List<String> expectedOptionList = dropdownOptions;
 
-       Assert.assertEquals(expectedOptionList,actualOptionList);
+        Assert.assertEquals(expectedOptionList, actualOptionList);
     }
 
     @Then("transactions table headers should be as following")
@@ -61,7 +62,7 @@ public class AccountActivityStepDef {
         List<String> actualHeaderList = BrowserUtils.getElementsText(options);
         List<String> expectedHeaderList = headers;
 
-        Assert.assertEquals(expectedHeaderList,actualHeaderList);
+        Assert.assertEquals(expectedHeaderList, actualHeaderList);
     }
 
     @Given("the user is logged in")
@@ -71,7 +72,7 @@ public class AccountActivityStepDef {
         LoginPage loginPage = new LoginPage();
         String username = ConfigurationReader.get("username");
         String password = ConfigurationReader.get("password");
-        loginPage.login(username,password);
+        loginPage.login(username, password);
 
     }
 
@@ -80,7 +81,7 @@ public class AccountActivityStepDef {
         String expectedTitle = "Zero - Account Activity";
         String actualTitle = Driver.get().getTitle();
 
-        Assert.assertEquals(expectedTitle,actualTitle);
+        Assert.assertEquals(expectedTitle, actualTitle);
     }
 
     @Then("Account drop down should have {string} selected")
@@ -89,7 +90,7 @@ public class AccountActivityStepDef {
         String selectedOption = accountActivityPage.accountDropdownList().getFirstSelectedOption().getText();
         String expectedOption = Option;
 
-        Assert.assertEquals(expectedOption,selectedOption);
+        Assert.assertEquals(expectedOption, selectedOption);
     }
 
     @Given("the user accesses the {string} tab")
@@ -107,6 +108,7 @@ public class AccountActivityStepDef {
 
     @When("clicks search")
     public void clicks_search() {
+        BrowserUtils.waitFor(1);
         new AccountActivityPage().findButton.click();
     }
 
@@ -119,7 +121,6 @@ public class AccountActivityStepDef {
         Date ndate2 = formatter1.parse(date2);
 
         List<String> actualDateList = BrowserUtils.getElementsText(new AccountActivityPage().dateColumnList);
-        System.out.println("actualDateList = " + actualDateList);
         List<Date> actualDates = new ArrayList<>();
         for (String dates : actualDateList) {
             Date ad = formatter1.parse(dates);
@@ -127,15 +128,15 @@ public class AccountActivityStepDef {
         }
 
         int counter = 0;
-        for(int i=0; i<actualDates.size(); i++){
-            if(ndate1.compareTo(actualDates.get(i))<=0 && ndate2.compareTo(actualDates.get(i))>=0){
+        for (int i = 0; i < actualDates.size(); i++) {
+            if (ndate1.compareTo(actualDates.get(i)) <= 0 && ndate2.compareTo(actualDates.get(i)) >= 0) {
                 counter++;
             }
         }
 
         boolean flag = false;
-        if(counter==actualDates.size()){
-            flag =true;
+        if (counter == actualDates.size()) {
+            flag = true;
         }
 
         Assert.assertTrue(flag);
@@ -180,28 +181,157 @@ public class AccountActivityStepDef {
     public void the_results_should_be_sorted_by_most_recent_date() {
         BrowserUtils.waitFor(1);
         List<String> actualDateList = BrowserUtils.getElementsText(new AccountActivityPage().dateColumnList);
-        System.out.println("actualDateList = " + actualDateList);
 
-        List<Integer> newList = new ArrayList<>();
-        for(String actualDate : actualDateList){
-            int dates  = Integer.parseInt(actualDate.replace("-",""));
-            System.out.println("dates = " + dates);
-            newList.add(dates);
+        //My original list is in decending order
+        //I use sort to make sure my list is in order, bu it gives me acsending order
+        Collections.sort(actualDateList);
+
+        //And I reverse it to compare in decending order
+        Collections.reverse(actualDateList);
+
+        List<String> reversedList = actualDateList;
+
+        boolean flag = true;
+        for (int i = 0; i < actualDateList.size(); i++) {
+            if (!actualDateList.get(i).equals(reversedList.get(i))) {
+                flag = false;
+                break;
+            }
+        }
+        Assert.assertTrue(flag);
+    }
+
+    @Then("clear the table")
+    public void clear_the_table() {
+        AccountActivityPage accountActivityPage = new AccountActivityPage();
+        accountActivityPage.fromDate.clear();
+        accountActivityPage.toDate.clear();
+    }
+
+    @Then("the results table should only not contain transactions dated {string}")
+    public void the_results_table_should_only_not_contain_transactions_dated(String date) {
+        BrowserUtils.waitFor(1);
+        List<String> actualDateList = BrowserUtils.getElementsText(new AccountActivityPage().dateColumnList);
+
+        boolean flag = true;
+        for (int i = 0; i < actualDateList.size(); i++) {
+            if (actualDateList.get(i).equals(date)) {
+                flag = false;
+                break;
+            }
+        }
+        Assert.assertTrue(flag);
+    }
+
+    @When("the user enters description inputbox {string}")
+    public void the_user_enters_description_inputbox(String input) {
+        BrowserUtils.waitFor(1);
+        AccountActivityPage accountActivityPage = new AccountActivityPage();
+        accountActivityPage.descriptionBox.clear();
+        accountActivityPage.descriptionBox.sendKeys(input);
+        accountActivityPage.findButton.click();
+
+    }
+
+    @Then("results table should only show descriptions containing {string}")
+    public void results_table_should_only_show_descriptions_containing(String str) {
+        BrowserUtils.waitFor(1);
+        AccountActivityPage accountActivityPage = new AccountActivityPage();
+        List<String> descriptionText = BrowserUtils.getElementsText(accountActivityPage.descriptionColumnList);
+
+        boolean flag = true;
+        for (int i = 0; i < descriptionText.size(); i++) {
+            if (!descriptionText.get(i).contains(str)) {
+                flag = false;
+                System.out.println("unmaching descriptionText = " + descriptionText.get(i));
+            }
         }
 
-        Collections.sort(newList);
-        System.out.println("newList = " + newList);
+        Assert.assertTrue(flag);
+    }
 
-        Collections.reverse(newList);
-        System.out.println("newList = " + newList);
+    @Then("results table should not show descriptions containing {string}")
+    public void results_table_should_not_show_descriptions_containing(String str) {
+        BrowserUtils.waitFor(1);
+        AccountActivityPage accountActivityPage = new AccountActivityPage();
+        List<String> descriptionText = BrowserUtils.getElementsText(accountActivityPage.descriptionColumnList);
+
+        boolean flag = true;
+        for (int i = 0; i < descriptionText.size(); i++) {
+            if (descriptionText.get(i).contains(str)) {
+                flag = false;
+                System.out.println("unmaching descriptionText = " + descriptionText.get(i));
+                break;
+            }
+        }
+        Assert.assertTrue(flag);
+    }
+
+    @Then("results table should show at least one result under Deposit")
+    public void results_table_should_show_at_least_one_result_under_Deposit() {
+        BrowserUtils.waitFor(1);
+        AccountActivityPage accountActivityPage = new AccountActivityPage();
+        List<String> actualresult = BrowserUtils.getElementsText(accountActivityPage.depositColumnList);
+
+        int count = 0;
+        for (String str : actualresult) {
+            if(!str.isEmpty()){
+                count++;
+            }
+        }
+        Assert.assertTrue(count >= 1);
+    }
+
+    @Then("results table should show at least one result under Withdrawal")
+    public void results_table_should_show_at_least_one_result_under_Withdrawal() {
+        BrowserUtils.waitFor(1);
+        AccountActivityPage accountActivityPage = new AccountActivityPage();
+        List<String> actualresult = BrowserUtils.getElementsText(accountActivityPage.withdrawalColumnList);
+
+        int count = 0;
+        for (String str : actualresult) {
+            if(!str.isEmpty()){
+                count++;
+            }
+        }
+        Assert.assertTrue(count >= 1);
+    }
+
+    @When("user selects type {string}")
+    public void user_selects_type(String option) {
+        AccountActivityPage accountActivityPage = new AccountActivityPage();
+        accountActivityPage.selectTypeList().selectByVisibleText(option);
+        accountActivityPage.findButton.click();
 
     }
 
-    @Then("the results table should only not contain transactions dated “{int}{int}{int}”")
-    public void the_results_table_should_only_not_contain_transactions_dated(Integer int1, Integer int2, Integer int3) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    @Then("results table should show no result under Withdrawal")
+    public void results_table_should_show_no_result_under_Withdrawal() {
+        BrowserUtils.waitFor(1);
+        AccountActivityPage accountActivityPage = new AccountActivityPage();
+        List<String> actualresult = BrowserUtils.getElementsText(accountActivityPage.withdrawalColumnList);
+
+        int count = 0;
+        for (String str : actualresult) {
+            if(!str.isEmpty()){
+                count++;
+            }
+        }
+        Assert.assertTrue(count == 0);
     }
 
+    @Then("results table should show no result under Deposit")
+    public void results_table_should_show_no_result_under_Deposit() {
+        BrowserUtils.waitFor(1);
+        AccountActivityPage accountActivityPage = new AccountActivityPage();
+        List<String> actualresult = BrowserUtils.getElementsText(accountActivityPage.depositColumnList);
 
+        int count = 0;
+        for (String str : actualresult) {
+            if(!str.isEmpty()){
+                count++;
+            }
+        }
+        Assert.assertTrue(count == 0);
+    }
 }
